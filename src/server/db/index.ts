73fac6cb2974a8ef152de7fbecb69334a5ce1d2d
Ivan B <sensenabi@gmail.com>
2023-12-15 16:01:40 -0500

    Initial commit

---
+++ src/server/db/index.ts
@@ -0,0 +1,18 @@
+import { Client } from "@planetscale/database";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+
+import { env } from "@/env";
+import * as schema from "./schema";
+
+export const db = drizzle(
+  new Client({
+    url: env.DATABASE_URL,
+    fetch: (url, init) => {
+      if (init) {
+        delete init.cache
+      }
+      return fetch(url, init);
+    },
+  }).connection(),
+  { schema },
+);
